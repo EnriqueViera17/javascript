1e28b379cc6ffa7d4ecc81fb2b62f4d9eea30f84
Fernando Jose <delgadof@seattleu.edu>
2015-09-16 23:41:20 -0400

    initial commit

---
+++ script.js
@@ -0,0 +1,8 @@
+function changeImage() {
+    var image = document.getElementById('myImage');
+    if (image.src.match("bulbon")) {
+        image.src = "images/pic_bulboff.gif";
+    } else {
+        image.src = "images/pic_bulbon.gif";
+    }
+}
