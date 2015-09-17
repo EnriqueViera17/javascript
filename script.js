7a62a52a0fe4cbc36ccddf07f4dba7f1231e07d3
Enrique Viera <viera.enrique17@csj-rpi.org>
2015-09-17 09:34:38 -0400

    Update script.js

--- script.js
+++ script.js
@@ -2,8 +2,8 @@ function changeImage() {
   var image = document.getElementById('myImage');
   
   if (image.src.match("bulbon")) {
-      image.src = "image/pic_bulboff.gif"
+      image.src = "images/pic_bulboff.gif"
     } else {   
-      image.src = "image/pic_bulbon.gif";
+      image.src = "images/pic_bulbon.gif";
     }
 }
